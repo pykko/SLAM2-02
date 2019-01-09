c6806634ceb8d0742aea6557c6f01a771fe5abd7
pykko <pykko@xilim.fr>
2019-01-09 17:21:57 +0100

    Intial

---
+++ CreationDates.java
@@ -0,0 +1,21 @@
+public class CreationDates {
+	
+
+	public static void main(String [] args){
+	
+		DateFr d1 ;
+		DateFr d2 ;
+		DateFr d3 ;
+		
+		// Votre code ici pour la création de la première date
+		//System.out.println( "d1 : " + d1.enChaine() ) ;
+		
+		// Votre code ici pour la création de la deuxième date
+		//System.out.println( "d2 : " + d2.enChaine() ) ;
+		
+		// Votre code ici pour la création de la troisième date
+		//System.out.println( "d3 : " + d3.enChaine() ) ;
+	
+	}
+
+}
---
+++ ManipulationDates.java
@@ -0,0 +1,40 @@
+public class ManipulationDates {
+
+	public static void main(String [] args){
+	
+		DateFr dateCourante , dateFinJuillet ;
+		boolean estAvant , estApres , estIdentique ;
+		
+		dateCourante = new DateFr( 4 , 7 , 2018 ) ;
+		dateFinJuillet = new DateFr( 31 , 7 , 2018 ) ;
+		
+		// Ici, l'instruction de la question 4
+		// System.out.println( "[Question 4] Date manipulée : " + ... ) ;
+		
+		// Ici, l'instruction de la question 5
+		//System.out.println( "[Question 5] Numéro du jour dans le mois : " + numeroJourDansMois ) ;
+		
+		// Ici, l'instruction de la question 6
+		//System.out.println( "[Question 6] Numéro du mois : " + numeroMois ) ;
+		
+		// Ici, l'instruction de la question 7
+		//System.out.println( "[Question 7] Date manipulée : " + ... ) ;
+		
+		// Ici, l'instruction de la question 8
+		//System.out.println( "[Question 8] Date manipulée : " + ... ) ;
+		
+		// Ici, l'instruction de la question 9
+		//System.out.println( "[Question 9] Date manipulée : " + ... ) ;
+		
+		// Ici, l'instruction de la question 10
+		//System.out.println( "[Question 10] La date est antérieure au 31/07/2018 : " + estAvant ) ;
+		
+		// Ici, l'instruction de la question 11
+		//System.out.println( "[Question 11] La date est postérieure au 31/07/2018 : " + estApres ) ;
+		
+		// Ici, l'instruction de la question 12
+		//System.out.println( "[Question 12] La date est le 01/08/2018 : " + estIdentique ) ;
+	
+	}
+
+}
